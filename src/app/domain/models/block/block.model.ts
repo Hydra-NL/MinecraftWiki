0981c0e2b699daff3f193f9bb041e8500495762b
Pascal <p.vandekeere@student.avans.nl>
2022-11-17 08:51:58 +0100

    Add-block

--- src/app/domain/models/block/block.model.ts
+++ src/app/domain/models/block/block.model.ts
@@ -10,7 +10,7 @@ export class Block extends Entity {
   name: string = '';
   description: string = '';
   stackable: boolean = false;
-  stackSize: number = 0; // 1 - 64
+  stackSize: number = 1; // 1 - 64
   hardness: number = 0; // 0 = hand, 1 = wood, 2 = stone, 3 = iron, 4 = diamond, 5 = netherite, 6 = unbreakable
   tool: ToolType = ToolType.pickaxe;
   biome: Biome = new Biome('');
--- src/app/navbar/nav.component.ts
+++ src/app/navbar/nav.component.ts
@@ -6,12 +6,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   darkTheme: boolean = false;
+  currentTime: Date = new Date();
+
   constructor() {
     console.log('NavComponent constructor');
   }
 
   ngOnInit() {
     console.log('NavComponent ngOnInit');
+    if (this.currentTime.getHours() >= 18 || this.currentTime.getHours() <= 6) {
+      this.switchTheme();
+    }
   }
 
   switchTheme() {
